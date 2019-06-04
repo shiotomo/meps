dbe1920b6a4e647e96abf2431a3f7a4138d36c8b
shiotomo <t.shiota16@gmail.com>
2019-06-05 02:50:17 +0900

    フォルダ構成を変更した

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar
+wget -P ./minecraft https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar
