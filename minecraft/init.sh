fb2a172e106f5c1642d4d63a86a400f906b19636
shiotomo <t.shiota16@gmail.com>
2020-01-16 01:31:42 +0900

    バージョンを更新した

--- minecraft/init.sh
+++ minecraft/init.sh
@@ -10,5 +10,5 @@ else
   # wget -P . https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar
 
   # v1.15
-  wget -P https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar
+  wget -P . https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar
 fi
