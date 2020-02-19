6ce2d7507e456f634b622d0ea2bbbf84103ebb48
shiotomo <t.shiota16@gmail.com>
2020-02-19 15:30:01 +0900

    docker_container.rbのtestを追加した

---
+++ spec/lib/docker_container_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../spec_helper'
+require_relative '../../lib/docker_container'
+
+RSpec.describe 'docker_container' do
+  context 'get_status' do
+    it 'statusのkey情報が正しい' do
+      # msnsのカレントディレクトリを取得
+      dir = File.expand_path('.')
+      status = DockerContainer.get_status(dir)
+      expect(status.has_key?(:msns_api)).to eq true
+      expect(status.has_key?(:msns_mc_server)).to eq true
+    end
+  end
+end
