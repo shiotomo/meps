73a9f7d63da3c4dfd00cad14821b35f440b5ab5f
shiotomo <t.shiota16@gmail.com>
2020-02-19 15:39:25 +0900

    docker_container.rbのtestを追加した

--- spec/lib/docker_container_spec.rb
+++ spec/lib/docker_container_spec.rb
@@ -10,5 +10,15 @@ RSpec.describe 'docker_container' do
       expect(status.has_key?(:msns_api)).to eq true
       expect(status.has_key?(:msns_mc_server)).to eq true
     end
+
+    it 'statusのvalue情報がactiveかinactiveである' do
+      # msnsのカレントディレクトリを取得
+      dir = File.expand_path('.')
+      status = DockerContainer.get_status(dir)
+      msns_api = status[:msns_api]
+      msns_mc_server = status[:msns_mc_server]
+      expect(msns_api).to match /(^active$|^inactive$)/
+      expect(msns_mc_server).to match /(^active$|^inactive$)/
+    end
   end
 end
