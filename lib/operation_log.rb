59b28435a1f599abaad8d0b6497dfe8590213ad5
shiotomo <t.shiota16@gmail.com>
2020-03-17 22:26:55 +0900

    operation_logを取得する時日付を指定できるようにした

--- lib/operation_log.rb
+++ lib/operation_log.rb
@@ -1,9 +1,16 @@
 require_relative './common/mysql_client'
+require_relative './common/time_converter'
 
 class OperationLog
+  ERROR_MESSAGE = {status: "Invalid date format."}
+
   class << self
     # 全operation_logを取得する
-    def get_operation_log
+    def get_operation_log(date)
+      return date == nil ? get_operation_log_all() : get_operation_log_where_date(date)
+    end
+
+    def get_operation_log_all
       client = MysqlClient.new
       query = "SELECT id, caused_at, level, log, created_at FROM operation_log"
       result = client.run_query(query)
@@ -14,7 +21,16 @@ class OperationLog
       return result_list
     end
 
-    def get_operation_log_where_date
+    def get_operation_log_where_date(date)
+      return ERROR_MESSAGE unless TimeConverter.is_mysql_time_stamp(date) 
+      client = MysqlClient.new
+      query = "SELECT id, caused_at, level, log, created_at FROM operation_log where created_at >= '#{date}'"
+      result = client.run_query(query)
+      result_list = []
+      result.each do |entry|
+        result_list.push(entry)
+      end
+      return result_list
     end
   end
 end
