322ff1029ffab0c5b94f26b1fd5227f492ac6596
shiotomo <t.shiota16@gmail.com>
2020-03-19 02:08:30 +0900

    logを複数のテーブルで管理できるようにした

--- db/migration/v1_0_setup.sql
+++ db/migration/v1_0_setup.sql
@@ -1,6 +1,14 @@
 CREATE DATABASE msns;
 use msns;
-CREATE TABLE IF NOT EXISTS `operation_log` (
+CREATE TABLE IF NOT EXISTS `access_log` (
+    `id` BIGINT AUTO_INCREMENT,
+    `caused_at` VARCHAR(256),
+    `level` VARCHAR(256),
+    `log` VARCHAR(256),
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
+CREATE TABLE IF NOT EXISTS `minecraft_log` (
     `id` BIGINT AUTO_INCREMENT,
     `caused_at` VARCHAR(256),
     `level` VARCHAR(256),
