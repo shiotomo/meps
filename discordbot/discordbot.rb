36673feea598e17df55cc6cdee99872854bdfa54
shiotomo <t.shiota16@gmail.com>
2020-06-15 01:50:11 +0900

    Discordから累計アクセス数を確認できるようにした

--- discordbot/discordbot.rb
+++ discordbot/discordbot.rb
@@ -23,6 +23,8 @@ bot.command :help do |event|
   "    nameに指定したlistを表示します。 例) !get whitelist\n" +
   "!nowlogin\n" +
   "    現在サーバにログインしているユーザを表示します。\n"
+  "!count\n" +
+  "    全ユーザの累計アクセス数を表示します。\n"
   event.send_message(message)
 end
 
@@ -34,7 +36,13 @@ end
 
 bot.command :nowlogin do |event|
   message = event.message.to_s.split(" ")
-  data = MinecraftServerAnalysis.get_access_count_where_now
+  data = MinecraftServerAnalysis.get_access_where_now
+  event.send_message(data)
+end
+
+bot.command :count do |event|
+  message = event.message.to_s.split(" ")
+  data = MinecraftServerAnalysis.get_access_count
   event.send_message(data)
 end
 
--- discordbot/lib/minecraft_server_analysis.rb
+++ discordbot/lib/minecraft_server_analysis.rb
@@ -3,22 +3,32 @@ require_relative './request_client'
 class MinecraftServerAnalysis
   class << self
     # サーバに現在ログインしている人を返却する
-    def get_access_count_where_now
+    def get_access_where_now
       url = ENV['API_URL'] + ":" + ENV['API_PORT'] + '/api/v1/access/now'
       headers = {Authorization: ENV['API_TOKEN']}
       body   =  {}
       request_client = RequestClient.new(url , headers, body)
       res = request_client.get()
-      return convert_slack_format(res)
+      return convert_discord_format(res)
     end
 
+    def get_access_count
+      url = ENV['API_URL'] + ":" + ENV['API_PORT'] + '/api/v1/access/count'
+      headers = {Authorization: ENV['API_TOKEN']}
+      body   =  {}
+      request_client = RequestClient.new(url , headers, body)
+      res = request_client.get()
+      return convert_discord_format(res)
+    end
+
+    private
     # JSONデータをSlack上で見やすい表記に変換する
-    def convert_slack_format(json_data)
+    def convert_discord_format(json_data)
       # TODO: 一旦そのまま返却する
       message = "== connect account list ==\n"
       login_users = ""
       json_data.each do |key, value|
-        login_users += "#{key}\n" if value
+        login_users += "#{key}: #{value}\n"
       end
       login_users += "No users on the server" if login_users == ""
       message += login_users
