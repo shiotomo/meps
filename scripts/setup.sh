5f832772872d8485843378dd612d8ddec491abb1
shiotomo <t.shiota16@gmail.com>
2020-07-12 22:57:27 +0900

    バージョン1.16.2に対応した

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -24,7 +24,11 @@ if [ -e $file ]; then
   exit
 fi
 
-if [ "$MINECRAFT_VERSION" = "1.15" ]; then
+if [ "$MINECRAFT_VERSION" = "1.14" ]; then
+  echo 'Install Version 1.14'
+  wget -P ./minecraft https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar
+  echo "Complete !!"
+elif [ "$MINECRAFT_VERSION" = "1.15" ]; then
   echo 'Install Version 1.15'
   wget -P ./minecraft https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar
   echo "Complete !!"
@@ -32,9 +36,9 @@ elif [ "$MINECRAFT_VERSION" = "1.15.2" ]; then
   echo 'Install Version 1.15.2'
   wget -P ./minecraft https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
   echo "Complete !!"
-elif [ "$MINECRAFT_VERSION" = "1.14" ]; then
-  echo 'Install Version 1.14'
-  wget -P ./minecraft https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar
+elif [ "$MINECRAFT_VERSION" = "1.16.1" ]; then
+  echo 'Install Version 1.16.1'
+  wget -P ./minecraft https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar
   echo "Complete !!"
 else
   echo '.envのMINECRAFT_VERSIONが無効です。'
