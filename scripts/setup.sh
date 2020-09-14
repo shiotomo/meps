4303bd423795c076908ec151f80d29343e5eb766
shiotomo <t.shiota16@gmail.com>
2020-09-14 20:24:21 +0900

    バージョン1.16.2に対応した

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -40,6 +40,10 @@ elif [ "$MINECRAFT_VERSION" = "1.16.1" ]; then
   echo 'Install Version 1.16.1'
   wget -P ./minecraft https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar
   echo "Complete !!"
+elif [ "$MINECRAFT_VERSION" = "1.16.2" ]; then
+  echo 'Install Version 1.16.2'
+  wget -P ./minecraft https://launcher.mojang.com/v1/objects/c5f6fb23c3876461d46ec380421e42b289789530/server.jar
+  echo "Complete !!"
 else
   echo '.envのMINECRAFT_VERSIONが無効です。'
   exit 1
